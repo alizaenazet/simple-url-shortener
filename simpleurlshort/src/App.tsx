81dacb55378c5f5c2869f2daaa6b2ef15a5cde7e
Nessaa16 <120008781+Nessaa16@users.noreply.github.com>
2025-05-29 21:33:33 +0700

    feat: create dashboard ui

--- simpleurlshort/src/App.tsx
+++ simpleurlshort/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/login'
 import Register from './components/register'
 import Shortener from './components/shortener'
+import Dashboard from './components/dashboard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Shortener />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
